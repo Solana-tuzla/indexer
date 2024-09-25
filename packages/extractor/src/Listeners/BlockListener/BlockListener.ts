5909272c4b1b91ff27e76ab0e8d9afe9d6b0453a
karlo292 <karlito1501@gmail.com>
2024-09-25 19:17:40 +0200

    made it so finalized slots won't display more than once in console

--- packages/extractor/src/Listeners/BlockListener/BlockListener.ts
+++ packages/extractor/src/Listeners/BlockListener/BlockListener.ts
@@ -6,17 +6,20 @@ export class BlockListener {
     _httpsUrl: string;
     _connection: web3.Connection;
     _blockData: any; // Variable to store block data
+    _finalizedSlots: Array<any> = []; // Variable to store finalized slots to avoid duplicates
+
 
     /** Constructor */
     constructor(wssUrl: string, httpsUrl: string) {
         this._wssUrl = wssUrl;
         this._httpsUrl = httpsUrl;
         this._connection = new web3.Connection(this._httpsUrl, { wsEndpoint: this._wssUrl });
+        this._finalizedSlots = [];
     }
 
     async listen() {
         this._connection.onSlotUpdate(async (slotUpdate) => {
-            if (slotUpdate.type === 'root') {
+            if (slotUpdate.type === 'root' || slotUpdate.type === 'optimisticConfirmation') {
                 // console.log(`Slot finalized: ${slotUpdate.slot}`);
                 
                 await this.fetchBlockDataWithRetry(slotUpdate.slot);
@@ -40,17 +43,31 @@ export class BlockListener {
 
                 // If block data is available, store it and return
                 if (block) {
+                    
+                    if (this._finalizedSlots.includes(slot)) {
+                        return;
+                    }
+                    this._finalizedSlots.push(slot);
+
+                    // Makes sure that _finalizedSlots doesn't have more than 20 elements
+                    if (this._finalizedSlots.length >= 20) {
+                        this._finalizedSlots.splice(0, 1);
+                    }
+
                     this._blockData = block;
                     
 
                     console.log(`New block received from slot ${slot}: ` + this._blockData);
-
+                    const date = new Date();
+                    const formattedDate = `${date.getUTCDate()}/${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}:${date.getUTCMilliseconds()}`;
+                    //console.log('Block finalized at: ' + formattedDate);
+                    
                     return;
                 }
             } catch (error) {
                 // If error -32004 happens (block data not available yet), this logs a warning
                 if ((error as { code: number }).code === -32004) {
-                    console.warn(`Block data not available yet for slot: ${slot}, attempt: ${attempt}`);
+                    //console.warn(`Block data not available yet for slot: ${slot}, attempt: ${attempt}`);
                 } else {
                     console.error("Error fetching block data:", error);
                     return;
